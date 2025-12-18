e5665459c0d3dff0e8c39fb1efb9a4b9a6e12319
Kinan-absar <kinan@absar-alomran.com>
2025-12-19 00:14:39 +0300

    adding balance

--- custom_addons/account_statement_reports/models/res_partner.py
+++ custom_addons/account_statement_reports/models/res_partner.py
@@ -4,6 +4,35 @@ from odoo import models
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
+# 🔽 ADD THIS FIELD
+    statement_balance = fields.Monetary(
+        string="Statement Balance",
+        compute="_compute_statement_balance",
+        currency_field="currency_id",
+    )
+
+    # 🔽 ADD THIS METHOD
+    def _compute_statement_balance(self):
+        for partner in self:
+            balance = 0.0
+
+            aml = self.env["account.move.line"].search([
+                ("partner_id", "=", partner.id),
+                ("parent_state", "=", "posted"),
+                ("account_id.account_type", "in", (
+                    "asset_receivable",
+                    "liability_payable",
+                )),
+            ])
+
+            for line in aml:
+                if line.account_id.account_type == "asset_receivable":
+                    balance += line.debit - line.credit
+                else:
+                    balance += line.credit - line.debit
+
+            partner.statement_balance = balance
+
     def action_open_customer_statement_wizard(self):
         self.ensure_one()
         return {
