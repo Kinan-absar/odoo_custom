7e2141eac4198ed5de8778a703b15c021e64e7bd
Kinan-absar <kinan@absar-alomran.com>
2026-01-04 09:55:43 +0300

    Update purchase_order.py

--- custom_addons/po_payment_link/models/purchase_order.py
+++ custom_addons/po_payment_link/models/purchase_order.py
@@ -5,38 +5,40 @@ class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
 
     paid_amount = fields.Monetary(
-        string="Paid Amount",
         compute="_compute_payment_status",
+        store=True,
     )
+
     remaining_amount = fields.Monetary(
-        string="Remaining Amount",
         compute="_compute_payment_status",
+        store=True,
     )
+
     payment_status = fields.Selection(
         [
             ("not_paid", "Not Paid"),
             ("partial", "Partially Paid"),
             ("paid", "Paid"),
         ],
-        string="Payment Status",
         compute="_compute_payment_status",
+        store=True,
     )
+
 
     @api.depends("amount_total")
     def _compute_payment_status(self):
         for po in self:
             paid = 0.0
 
-            payments = self.env["account.payment"].search([
-                ("state", "=", "posted"),
-                ("purchase_id", "=", po.id),
+            move_lines = self.env["account.move.line"].search([
+                ("move_id.state", "=", "posted"),
+                ("account_id.account_type", "=", "liability_payable"),
+                ("move_id.purchase_id", "=", po.id),
                 ("company_id", "=", po.company_id.id),
             ])
 
-            for payment in payments:
-                for line in payment.move_id.line_ids:
-                    if line.account_id.account_type == "liability_payable":
-                        paid += abs(line.balance)
+            for line in move_lines:
+                paid += abs(line.balance)
 
             po.paid_amount = paid
             po.remaining_amount = po.amount_total - paid
