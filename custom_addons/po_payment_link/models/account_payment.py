909bfb868602ef7c4b13aeef100a540dba05d41e
Kinan-absar <kinan@absar-alomran.com>
2026-01-04 09:28:34 +0300

    sd

--- custom_addons/po_payment_link/models/account_payment.py
+++ custom_addons/po_payment_link/models/account_payment.py
@@ -1,5 +1,12 @@
 from odoo import models, fields
 
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    purchase_id = fields.Many2one(
+        "purchase.order",
+        string="Related Purchase Order",
+    )
 class AccountPayment(models.Model):
     _inherit = "account.payment"
 
@@ -8,3 +15,10 @@ class AccountPayment(models.Model):
         string="Related Purchase Order",
         domain="[('partner_id', '=', partner_id), ('state', 'in', ('purchase','done'))]",
     )
+
+    def action_post(self):
+        res = super().action_post()
+        for payment in self:
+            if payment.purchase_id and payment.move_id:
+                payment.move_id.purchase_id = payment.purchase_id
+        return res
