00eb349bf298fa1d9521bb82689ab3921c91c702
Kinan-absar <kinan@absar-alomran.com>
2026-02-19 21:16:02 +0300

    asd

--- custom_addons/petty_cash_management/controllers/portal_petty_cash.py
+++ custom_addons/petty_cash_management/controllers/portal_petty_cash.py
@@ -35,3 +35,36 @@ class PortalPettyCash(CustomerPortal):
             'petty_cash_management.portal_petty_cash_detail',
             values
         )
+from odoo import http
+from odoo.http import request
+
+class PettyCashPortal(http.Controller):
+
+    @http.route('/my/petty-cash/new', type='http', auth='user', website=True)
+    def portal_petty_cash_new(self, **kw):
+
+        return request.render(
+            'petty_cash_management.portal_petty_cash_form',
+            {}
+        )
+    @http.route('/my/petty-cash/create', type='http', auth='user', website=True, methods=['POST'])
+    def portal_petty_cash_create(self, **post):
+
+        petty_cash = request.env['petty.cash'].sudo().create({
+            'user_id': request.env.user.id,
+            'date': post.get('date'),
+        })
+
+        return request.redirect(f'/my/petty-cash/{petty_cash.id}')
+    @http.route('/my/petty-cash/<int:report_id>', type='http', auth='user', website=True)
+    def portal_petty_cash_detail(self, report_id, **kw):
+
+        report = request.env['petty.cash'].sudo().browse(report_id)
+
+        if report.user_id.id != request.env.user.id:
+            return request.redirect('/my')
+
+        return request.render(
+            'petty_cash_management.portal_petty_cash_detail',
+            {'report': report}
+        )
