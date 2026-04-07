49c9a3025408b4ed36d9b246ad0eaf9b3e0c20da
Kinan-absar <kinan@absar-alomran.com>
2026-04-07 15:46:28 +0300

    asdsa

--- custom_addons/employee_portal_suite/__manifest__.py
+++ custom_addons/employee_portal_suite/__manifest__.py
@@ -81,6 +81,7 @@
         # --------------------------------------------------
         'views/employee_portal_layout.xml',
         'views/employee_dashboard_page.xml',
+        'views/employee_checkin_page.xml',
 
         # Employee Requests
         'views/employee_requests_page.xml',
