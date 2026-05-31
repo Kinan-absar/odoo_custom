c2dcd9e230e028ad531221df9e825dd6dfe5ac9d
Kinan-absar <kinan@absar-alomran.com>
2026-05-31 20:40:16 +0300

    update

--- custom_addons/employee_portal_suite/controllers/portal_redirect.py
+++ custom_addons/employee_portal_suite/controllers/portal_redirect.py
@@ -5,18 +5,28 @@ from odoo.addons.portal.controllers.portal import CustomerPortal
 
 class EmployeePortalRedirect(CustomerPortal):
 
-    @http.route(['/my', '/my/home'], type='http', auth='user', website=True)
+    @http.route(['/my'], type='http', auth='user', website=True)
     def account(self, **kw):
+        """Redirect employees away from customer portal."""
         user = request.env.user
-        partner = user.partner_id
 
-        # Vendors first
-        if partner.supplier_rank:
-            return request.redirect('/vendor/dashboard')
-
-        # Employees second
+        # Employees → always redirect to employee portal
         if user.employee_id:
             return request.redirect('/my/employee')
 
-        # Customers
+        # Customers/vendors → normal Odoo portal
         return super().account(**kw)
+
+
+    @http.route(['/my/home'], type='http', auth='user', website=True)
+    def home_redirect(self, **kw):
+        """Override My Account page. Employees should never land here."""
+        user = request.env.user
+
+        # Employees → force employee dashboard
+        if user.employee_id:
+            return request.redirect('/my/employee')
+
+        # Normal customers/vendors → default portal home
+        return super().account(**kw)
+    
