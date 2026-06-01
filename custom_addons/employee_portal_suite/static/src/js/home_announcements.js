daf3823ad267b815920a020c9934a6512b896ce8
Kinan-absar <kinan@absar-alomran.com>
2026-06-01 18:34:10 +0300

    kjh

--- custom_addons/employee_portal_suite/static/src/js/home_announcements.js
+++ custom_addons/employee_portal_suite/static/src/js/home_announcements.js
@@ -1,3 +1,38 @@
 /** @odoo-module **/
-// Announcements are now shown via the systray icon (announcement_systray.js).
-// This file is intentionally empty.
+
+import { patch } from "@web/core/utils/patch";
+import { HomeMenu } from "@web_enterprise/webclient/home_menu/home_menu";
+import { markup, onWillStart, useState } from "@odoo/owl";
+import { useService } from "@web/core/utils/hooks";
+
+patch(HomeMenu.prototype, {
+    setup() {
+        super.setup(...arguments);
+
+        this.orm = useService("orm");
+        this.backendAnnouncementState = useState({
+            announcements: [],
+            collapsed: false,   // panel starts open
+        });
+
+        onWillStart(async () => {
+            try {
+                const announcements = await this.orm.call(
+                    "portal.announcement",
+                    "get_backend_announcements",
+                    []
+                );
+                this.backendAnnouncementState.announcements = announcements.map((ann) => ({
+                    ...ann,
+                    message: markup(ann.message || ""),
+                }));
+            } catch (error) {
+                this.backendAnnouncementState.announcements = [];
+            }
+        });
+    },
+
+    toggleAnnouncements() {
+        this.backendAnnouncementState.collapsed = !this.backendAnnouncementState.collapsed;
+    },
+});
