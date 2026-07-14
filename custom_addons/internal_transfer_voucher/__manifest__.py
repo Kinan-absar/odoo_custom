20c254cfb8e00663321b61b92086a56928a36e48
Kinan-absar <kinan@absar-alomran.com>
2026-07-14 12:37:47 +0200

    lkh

--- custom_addons/internal_transfer_voucher/__manifest__.py
+++ custom_addons/internal_transfer_voucher/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Payment & Receipt Voucher",
-    "version": "18.0.1.5.0",
+    "version": "18.0.1.5.1",
     "category": "Accounting",
     "summary": "Internal journal transfers, payment vouchers, and receipt vouchers for Odoo 18",
     "description": """
