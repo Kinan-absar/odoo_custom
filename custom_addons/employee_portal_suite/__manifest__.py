bd3cbeea1d3812ffacb5c2ef25b32e68e8860fc2
Kinan-absar <kinan@absar-alomran.com>
2026-08-09 12:20:21 +0200

    update

--- custom_addons/employee_portal_suite/__manifest__.py
+++ custom_addons/employee_portal_suite/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Employee Portal Suite',
-    'version': '18.0.1.9.0',
+    'version': '18.0.1.10.0',
     'summary': 'Employee & Material Requests, GPS Attendance, Multi-Level Approvals, Employee Portal Suite',
 
     'description': """
