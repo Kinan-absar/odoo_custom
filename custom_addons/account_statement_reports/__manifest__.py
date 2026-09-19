76df7eb45e94aa7295bc38b2d460e00296b0e3c5
Kinan-absar <kinan@absar-alomran.com>
2026-09-19 17:33:09 +0200

    asd

--- custom_addons/account_statement_reports/__manifest__.py
+++ custom_addons/account_statement_reports/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     "name": "Account Statement Reports",
-    "version": "18.0.10.0.0",
+    "version": "18.0.11.0.0",
     "category": "Accounting",
     "summary": "Full-screen receivable and payable account statements with editable filters, PDF and Excel",
     "description": """
