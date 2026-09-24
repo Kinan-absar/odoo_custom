e7a094c8954ee11022c044f2be62a50bf798794f
Kinan-absar <kinan@absar-alomran.com>
2026-09-24 12:13:01 +0200

    s

--- custom_addons/construction_contract_management/__manifest__.py
+++ custom_addons/construction_contract_management/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Construction Contract Management',
-    'version': '18.0.1.5.1',
+    'version': '18.0.1.5.2',
     'summary': 'Dual-mode construction contract and subcontract management',
     'description': """
 Construction Contract Management
